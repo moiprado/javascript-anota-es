ebf3437a89172b17103cdd81ab6f3a00f5025cf4
moiprado <moiprado54@gmail.com>
2022-07-29 17:20:50 -0300

    initial commit

---
+++ aula.js
@@ -0,0 +1,42 @@
+// console.log('Moises')
+// console.log('bem vindo ap curso')
+
+// let idade = 5;
+// console.log(idade);
+
+// let altura = 190;
+// console.log(altura);
+// camelCase : serve para dar espaço sempre primeira letra minuscula ea outra maiuscula;
+
+// let idade = 7;
+// console.log(idade);
+
+// let marca = "Adidas";
+// console.log(marca);
+
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 30
+// console.log(valorIngressoAdulto);
+
+// Ferramenta const serve para mudar variaveis constate que muda o valor cada poco;
+
+// const valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+
+
+//TiPOS PRIMITIVOS;
+//Tipos de referencia;
+
+//let nome = 'Rafael'; //string"TEXTO" literal;
+//let idade = 34; //number literal;
+//let estaAprovado = true; //boolean serve para criar regras no site;
+//let sobrenome ; //Undefined é para uma variavel que nao tem definição;
+//let corselecionado = null //cor do fundo da pagina e quando for colocar a cor coloca '' aspas simples;
+
+//TIPAGEM MECANICA;
+
+//let nome = 'Rafael';  tipo é Number
+//let idade = 34; tipo é Number
+//let estaAprovado = true; 
+//let sobrenome ; 
+//let corselecionado = null
